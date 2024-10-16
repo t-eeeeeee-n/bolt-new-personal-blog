06eebedee991c62b4c62a30ada1ddf0b99dc5512
tensho.arai <tensho.arai@ities-inc.co.jp>
2024-10-16 17:48:00 +0900

    fix: design

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default  {
+  content: [
+    "./src/**/*.{html,js,astro}", // すべての必要なパスを追加
+    "./layouts/**/*.{astro,html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
